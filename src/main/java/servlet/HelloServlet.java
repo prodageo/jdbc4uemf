de31845f9733b485fbec8d6e598c50774b4441b9
fbab <fred@baucher.net>
2019-03-20 23:35:10 +0100

    inclus le code jdbc

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,6 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
@@ -17,28 +24,32 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws ServletException, IOException
+			
+	{
+        // URISyntaxException, SQLException 
             
-            
+		try {
+			
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-        Connection connection  DriverManager.getConnection(dbUrl, username, password);
-            
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-        while (rs.next()) {
-            // System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-        }
-            
-            
-            
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+			Connection connection = DriverManager.getConnection(dbUrl, username, password);
+				
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+			stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+			while (rs.next()) {
+				// System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+			}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}            
             
         ServletOutputStream out = resp.getOutputStream();
         out.write("hello heroku".getBytes());
