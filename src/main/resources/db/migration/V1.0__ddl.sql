796e676c9b89503b1872c529d0e95a2836c6ba03
Frédéric BAUCHER <fred@baucher.net>
2019-03-24 16:08:25 +0100

    Update V1.0__ddl.sql

--- src/main/resources/db/migration/V1.0__ddl.sql
+++ src/main/resources/db/migration/V1.0__ddl.sql
@@ -1,12 +1,5 @@
--- ********************* NE PAS EDITER **********************************
--- Il faut laisser la migration 1.0 à vide car elle n'est pas executée si   
--- <baselineOnMigrate>true</baselineOnMigrate>
--- http://prodageo.insa-rouen.fr/wiki/pmwiki.php?n=FilRouge.FlyWay
+DROP TABLE IF EXISTS TEST10 ;
 
--- create table FANTOME (
---    ID int not null,
--- );
-
-create table FANTOME10 (
+create table TEST10 (
     ID int not null
 );
