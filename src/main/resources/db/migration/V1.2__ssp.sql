66de5f2ba083aff566aca2b957eec131e8f6c82b
Frédéric BAUCHER <fred@baucher.net>
2019-03-27 18:56:38 +0100

    Update V1.2__ssp.sql

--- src/main/resources/db/migration/V1.2__ssp.sql
+++ src/main/resources/db/migration/V1.2__ssp.sql
@@ -1,1 +1,15 @@
-
+DROP FUNCTION IF EXISTS getMissionnaires2 ;
+CREATE FUNCTION getMissionnaires2 () 
+ RETURNS TABLE (
+ query_nom VARCHAR,
+ query_prenom VARCHAR
+) 
+AS $$
+BEGIN
+ RETURN QUERY SELECT
+ nom,
+ prenom
+ FROM
+ missionnaires ;
+END; $$ 
+LANGUAGE 'plpgsql';
