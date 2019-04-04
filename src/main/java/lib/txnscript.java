4d0b5c32c19a66223f6a2b465b3e1d0e0a2b0bb2
fbab <fred@baucher.net>
2019-04-05 01:59:57 +0200

    prise en charge commentaires JavaDoc

---
+++ src/main/java/lib/txnscript.java
@@ -0,0 +1,21 @@
+package lib ;
+
+public class TxnScript {
+
+ // des exemples de fonction attendu dans de ce fichier sont fournis dans
+ // http://prodageo.insa-rouen.fr/wiki/pmwiki.php?n=Umlp.EXU9912txnscript
+ /** 
+     * 
+     * cre'ation dans l'outil de persistence d'un enregistrement de re'servation
+	 * suite à la re'ception d'une demande par le controleur.
+     *
+     * @param id_associe    adresse de messagerie de l'associe qui effectue la réservation (ie : solange.smith@gmail.com)
+     * @param date_arrivee  date d'arrivée des amis de l'associe' dans la maison (ie : 23/07/2019)
+     * @return              true si la reque'te a re'ussi
+     */
+
+  public boolean creerEnrReservation ( String id_associe, DateTime date_arrivee ) 
+  {
+    return true ;
+  }
+}
