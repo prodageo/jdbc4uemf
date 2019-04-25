ed5c2ac5c43d5a008a6554811307f46ff2cdb930
Frédéric BAUCHER <fred@baucher.net>
2019-04-25 20:16:28 +0200

    Update txnscript.java

--- src/main/java/lib/txnscript.java
+++ src/main/java/lib/txnscript.java
@@ -1,10 +1,27 @@
 package lib ;
 
-public class TxnScript {
+public class txnscript {
 
  // des exemples de fonction attendu dans de ce fichier sont fournis dans
  // http://prodageo.insa-rouen.fr/wiki/pmwiki.php?n=Umlp.EXU9912txnscript
+
+
  /** 
+     * 
+     * cre'ation dans l'outil de persistence d'un enregistrement de re'servation
+	 * suite à la re'ception d'une demande par le controleur.
+     *
+     * @param id_associe    adresse de messagerie de l'associe qui effectue la réservation (ie : solange.smith@gmail.com)
+     * @param date_arrivee  date d'arrivée des amis de l'associe' dans la maison (ie : 23/07/2019)
+     * @return              true si la reque'te a re'ussi
+     */
+
+  public boolean creerAssocie ( String email_address, String name ) 
+  {
+    return 1 ;
+  }
+	
+     /** 
      * 
      * cre'ation dans l'outil de persistence d'un enregistrement de re'servation
 	 * suite à la re'ception d'une demande par le controleur.
