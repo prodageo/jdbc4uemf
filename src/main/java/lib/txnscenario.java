c4f492f03cecf424a21fab0dd79a52dc134472f1
BAUCHER Frederic <frederic.baucher@cegedim.fr>
2019-04-26 16:37:57 +0200

    toilettage pour meilleure lecture du code

---
+++ src/main/java/lib/txnscenario.java
@@ -0,0 +1,41 @@
+package lib ;
+
+import java.util.Date;
+import libinsa.txnscriptUtil ;
+import lib.txnscript ;
+
+public class txnscenario {
+
+ /** 
+     * 
+     * La fonction test doit réutiliser l'ensemble des opérations de la classe
+	 * dans un enchaînement et avec des valeurs de paramètres qui permettent 
+     * de réaliser un scénario réaliste. Par exemple :
+	 *   - créer un enregistrement
+	 *   - vérifier qu'une recherche renvoie bien les valeurs communiquées à la création
+	 *   - etc ...
+	 * 
+     * @return cette chaîne simule un écran 
+	 *         dans le but de stocker
+	 *         l'ensemble des chaines que l'on souhaiterait afficher
+     */
+ 
+  public String test()
+  {
+	String log = "" ;
+
+	// These 3 lines are only for running the examples
+	// If you are confident, you can comment it.
+	String the_message = "Run the example" + "\n" ; 		
+	txnscriptUtil txnUtil = new txnscriptUtil() ;
+	log = txnUtil.test () ;		
+
+	// ******************
+	// For INSA-ROUEN : put your the code here to call any operation defined in this class
+
+	
+	
+	// ******************
+	return log ;
+  }
+}
