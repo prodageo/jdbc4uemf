ebca113778c8d022c0b8584c706ab3ba9c9a4a3a
Frédéric BAUCHER <fred@baucher.net>
2019-04-26 06:57:49 +0200

    Update V1.0__ddl.sql

--- src/main/resources/db/migration/V1.0__ddl.sql
+++ src/main/resources/db/migration/V1.0__ddl.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS TEST10 ;
 DROP TABLE IF EXISTS TEST11 ;
 
+-- info : SERIAL is an INTEGER (from : https://www.postgresql.org/docs/9.1/datatype-numeric.html)
 create table TEST10 (
     ID SERIAL PRIMARY KEY,
     label10 VARCHAR(100) NOT NULL
