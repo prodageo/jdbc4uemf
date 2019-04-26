556cfa85b297c0aae34c1fba5656afea34a64dde
Frédéric BAUCHER <fred@baucher.net>
2019-04-26 06:55:06 +0200

    Update V1.0__ddl.sql

--- src/main/resources/db/migration/V1.0__ddl.sql
+++ src/main/resources/db/migration/V1.0__ddl.sql
@@ -2,9 +2,11 @@ DROP TABLE IF EXISTS TEST10 ;
 DROP TABLE IF EXISTS TEST11 ;
 
 create table TEST10 (
-    ID int not null
+    ID SERIAL PRIMARY KEY,
+    label10 VARCHAR(100) NOT NULL
 );
 
 create table TEST11 (
-    ID int not null
+    ID int not null,
+    label11 VARCHAR(100)
 );
