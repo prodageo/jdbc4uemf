db7a953261f5e5e82884b3252902fe972aca3aac
frederic-baucher <frederic.baucher@insa-rouen.fr>
2020-04-18 12:17:26 +0200

    Update logInsa.java

--- src/main/java/libinsa/logInsa.java
+++ src/main/java/libinsa/logInsa.java
@@ -1,10 +1,30 @@
 // wrapper to log system, namely Heroku, ...
 package libinsa;
 
-public class logInsa 
+public class insaLogger
 {
 
-  public static void log( String logMessage )
+    private static volatile insaLogger instance = null;
+     
+    // other fields / standard constructors / getters
+  
+    private insaLogger(String myName)
+    {
+        // this.name = myName;
+    }   
+     
+    public static insaLogger getLogger(String theClass) {
+        if (instance == null) {
+            synchronized (insaLogger.class) {
+                if (instance == null) {
+                    instance = new insaLogger(theClass);
+                }
+            }
+        }
+        return instance;
+    }
+  
+  public static void debug( String logMessage )
   {
     // https://devcenter.heroku.com/articles/logging
     System.err.println( logMessage );
